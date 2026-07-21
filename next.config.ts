e90d960f681856d81d42e8c5fcc66d47e0fc9bf7
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2026-07-21 09:34:17 +0530

    new implementation

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-  allowedDevOrigins: ["192.168.1.133"],
-};
-
-export default nextConfig;
